af4c74fcb6b90acb086140c1fa784161d4be043c
Nikita <gramps.master@gmail.com>
2015-02-15 20:41:53 -0500

    fixes

--- js/loadMaps.js
+++ js/loadMaps.js
@@ -3,18 +3,18 @@
  */
 function init_map() {
     var myOptions = {zoom: 14, center: new google.maps.LatLng(40.875843, -74.12205289999997), mapTypeId: google.maps.MapTypeId.ROADMAP};
-    map = new google.maps.Map(document.getElementById("map1"), myOptions);
+    map = new google.maps.Map(document.getElementById("map2"), myOptions);
     marker = new google.maps.Marker({map: map, position: new google.maps.LatLng(40.875843, -74.12205289999997)});
-    infowindow = new google.maps.InfoWindow({content: "<b>Modern Dental Vision / Magic Smile</b><br/>6 Village Square East<br/>07011 Clifton, NJ " });
+    infowindow = new google.maps.InfoWindow({content: "<b>Modern Dental Vision</b><br/>6 Village Square East<br/>07011 Clifton, NJ " });
     google.maps.event.addListener(marker, "click", function () {
         infowindow.open(map, marker);
     });
     infowindow.open(map, marker);
 
      var myOptions = {zoom: 14, center: new google.maps.LatLng(40.237028, -74.74830399999996), mapTypeId: google.maps.MapTypeId.ROADMAP};
-    map = new google.maps.Map(document.getElementById("map2"), myOptions);
+    map = new google.maps.Map(document.getElementById("map1"), myOptions);
     marker = new google.maps.Marker({map: map, position: new google.maps.LatLng(40.237028, -74.74830399999996)});
-    infowindow = new google.maps.InfoWindow({content: "<b>Modern Dental Vision / Magic Smile</b><br/>901 North Olden Avenue<br/>08638 Trenton,NJ " });
+    infowindow = new google.maps.InfoWindow({content: "<b>Modern Dental Vision</b><br/>901 North Olden Avenue<br/>08638 Trenton, NJ" });
     google.maps.event.addListener(marker, "click", function () {
         infowindow.open(map, marker);
     });
@@ -26,6 +26,10 @@ $(".service").click(function(){
     //alert($(this).data('open'));
     if(!$(this).data('open')){
         $(this).height("100%");
+        if($(this).data('img1'))
+            $(this).append("<div><img class='smallImage' src='"+ $(this).data('img1') +"'></div>");
+        if($(this).data('img2'))
+            $(this).append("<div><img class='smallImage' src='"+ $(this).data('img2') +"'></div>");
         $(this).append("<div><p class='info'>" + $(this).data('info') + "</p></div>");
     }
     else{
